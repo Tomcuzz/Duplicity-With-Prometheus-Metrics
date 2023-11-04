e8f75bfdc97bbe98d7aa96bd306d69a46f956ab9
Thomas Cousin <907403+Tomcuzz@users.noreply.github.com>
2023-11-04 15:06:48 +0000

    Fix to put run duplicity

--- code/duplicity.py
+++ code/duplicity.py
@@ -32,27 +32,77 @@ class Duplicity:
             self,
             backup_name="duplicity",
             pre_backup_date_file="",
-            restored_date_file=""):
+            restored_date_file="",
+            duplicity_full_if_older_than="",
+            duplicity_verbosity="",
+            duplicity_allow_source_mismatch=True,
+            duplicity_backup_method="ssh",
+            duplicity_ssh_port=22,
+            duplicity_ssh_key_file="/id_rsa",
+            duplicity_ssh_user="duplicity",
+            duplicity_ssh_host="192.168.1.1",
+            duplicity_remote_path="/home/duplicity/backup",
+            duplicity_ssh_strict_host_key_checking=False):
         self.backup_name = backup_name
         self.pre_backup_date_file = pre_backup_date_file
         self.restored_date_file = restored_date_file
+        self.duplicity_full_if_older_than = duplicity_full_if_older_than
+        self.duplicity_verbosity = duplicity_verbosity
+        self.duplicity_allow_source_mismatch = duplicity_allow_source_mismatch
+        self.duplicity_backup_method = duplicity_backup_method
+        self.duplicity_ssh_port = duplicity_ssh_port
+        self.duplicity_ssh_key_file = duplicity_ssh_key_file
+        self.duplicity_ssh_user = duplicity_ssh_user
+        self.duplicity_ssh_host = duplicity_ssh_host
+        self.duplicity_remote_path = duplicity_remote_path
+        self.duplicity_ssh_strict_host_key_checking = duplicity_ssh_strict_host_key_checking
 
-    def run_pre_backup(self):
+    def run_pre_backup(self) -> dict:
         """ Run pre backup processing. """
         return self.__write_duplicity_restore_test_file()
 
-    def run_backup(self):
+    def run_backup(self) -> dict:
         """ Run backup and return metrics. """
         logs = self.__capture_command_out(
-            command=[''], # command=['docker', 'logs', self.backup_name, "-n", "40"],
+            command=self.__build_duplicity_command(),
             print_output=True)
         return self.__process_duplicity_logs(logs)
+
+    def __build_duplicity_command(self) -> list:
+        """ Build the duplicity command. """
+        out = ["duplicity"]
+        if self.duplicity_full_if_older_than:
+            out.append("--full-if-older-than=" + self.duplicity_full_if_older_than)
+        if self.duplicity_verbosity:
+            out.append("--verbosity=" + self.duplicity_verbosity)
+        if self.duplicity_allow_source_mismatch:
+            out.append("--allow-source-mismatch")
+        if self.duplicity_backup_method == "ssh":
+            ssh_options = "--rsync-options='-e \"ssh "
+            ssh_options += " -p " + self.duplicity_ssh_port
+            ssh_options += " -i " + self.duplicity_ssh_key_file
+            if self.duplicity_ssh_strict_host_key_checking:
+                ssh_options += " -o StrictHostKeyChecking=yes"
+            else:
+                ssh_options += " -o StrictHostKeyChecking=no"
+            ssh_options += "\"'"
+            out.append(ssh_options)
+        out.append(" /home/duplicity/backup/data")
+        if self.duplicity_backup_method == "ssh":
+            rsync_location = "rsync://"
+            rsync_location += self.duplicity_ssh_user
+            rsync_location += "@"
+            rsync_location += self.duplicity_ssh_host
+            rsync_location += ":"
+            rsync_location += self.duplicity_remote_path
+            out.append(rsync_location)
+        return out
 
     def run_post_backup(self):
         """ Run post backup processing. """
         return self.__read_duplicity_restore_test_file()
 
-    def __capture_command_out(self, command, print_output=False):
+    def __capture_command_out(self, command:list, print_output=False) -> list:
         """ Runs a command on the command line and returns output. """
         proc = subprocess.Popen(command,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         out = []
@@ -65,7 +115,7 @@ class Duplicity:
             out.append(line.decode('utf-8'))
         return out
 
-    def __process_duplicity_logs(self, log_output):
+    def __process_duplicity_logs(self, log_output:list) -> dict:
         """ Process duplicity logs to extract metrics. """
         out = copy.deepcopy(metric_template)
 
@@ -107,7 +157,7 @@ class Duplicity:
 
         return out
 
-    def __write_duplicity_restore_test_file(self):
+    def __write_duplicity_restore_test_file(self) -> dict:
         """ Write a date file to check restore works correctly. """
         out = {
             "backup-test-file-date": 0,
@@ -121,7 +171,7 @@ class Duplicity:
 
         return out
 
-    def __read_duplicity_restore_test_file(self):
+    def __read_duplicity_restore_test_file(self) -> dict:
         """ Read a restore date file test file. """
         out = {
             "restore-file-date": 0,
@@ -131,7 +181,7 @@ class Duplicity:
         out["restore-file-read-success"] = out_temp[0]
         out["restore-file-date"] = out_temp[1]
 
-    def __read_date_file(self, location):
+    def __read_date_file(self, location:str) -> (bool, int):
         """ Read and process a date file. """
         try:
             restored_test_file = self.__capture_command_out(['cat', location])
@@ -141,6 +191,6 @@ class Duplicity:
             print("Caught Error While Processing Restore Date File: " + str(e))
         return False, 0
 
-    def __process_date_file(self, file_content):
+    def __process_date_file(self, file_content:str)  -> int:
         """ Process date file. """
         return int(datetime.strptime(file_content, "%a %d %b %H:%M:%S %Z %Y").timestamp())
