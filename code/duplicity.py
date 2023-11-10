f9b8e185b628906836dc55e8ad552902c676e4fe
Thomas Cousin <thomas.cousin@me.com>
2023-11-10 13:48:40 +0000

    Fix incorrect id setting

--- code/duplicity.py
+++ code/duplicity.py
@@ -214,8 +214,8 @@ class Duplicity:
             fp.write(datetime.now(pytz.utc).strftime("%a %d %b %H:%M:%S %Z %Y"))
 
         out_temp = self.__read_date_file(pre_backup_date_file)
-        out["restore-file-read-success"] = out_temp[0]
-        out["restore-file-date"] = out_temp[1]
+        out["backup-test-file-success"] = out_temp[0]
+        out["backup-test-file-date"] = out_temp[1]
         return out
 
     def __read_duplicity_restore_test_file(self) -> dict:
