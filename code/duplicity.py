94d9c118684c612fc5e5053f91c106cfe65b1ca1
Thomas Cousin <thomas.cousin@me.com>
2023-11-10 13:13:36 +0000

    Remove new line from command output print

--- code/duplicity.py
+++ code/duplicity.py
@@ -147,13 +147,13 @@ class Duplicity:
             if not line:
                 break
             if print_prefix:
-                print(print_prefix + ": " + line.decode('utf-8'))
+                print(print_prefix + ": " + line.decode('utf-8').strip())
             out.append(line.decode('utf-8'))
         while True:
             line = proc.stderr.readline()
             if not line:
                 break
-            print(print_prefix + "[COMMAND ERROR]" + ": " + line.decode('utf-8'))
+            print(print_prefix + "[COMMAND ERROR]" + ": " + line.decode('utf-8').strip())
         return out
 
     def __process_duplicity_logs(self, log_output:list) -> dict:
