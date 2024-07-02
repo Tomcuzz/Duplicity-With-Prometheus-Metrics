58759b9a3e2298accf685ee5a8ec721065965422
Thomas Cousin <thomas.cousin@me.com>
2024-07-02 16:11:42 +0100

    Exlude lost and found

--- code/duplicity.py
+++ code/duplicity.py
@@ -113,6 +113,7 @@ class Duplicity:
         if self.params.exclude_backup_dirs:
             for exclude_dir in self.params.exclude_backup_dirs.split(","):
                 out.append("--exclude=" + exclude_dir)
+        out.append("--exclude=/backup/data/lost+found")
         out.append(self.params.location_params.local_path)
         if self.params.backup_method == DuplicityBackupMethod.SSH:
             rsync_location = "rsync://"
